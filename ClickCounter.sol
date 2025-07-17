4e6c7241bb9a151d8047cf4b3ab63a030b6c5c70
Raqeeb Ameen <72813671+Ameen-Raqeeb@users.noreply.github.com>
2025-07-17 22:30:15 +0530

    Day 1
    Added some modifications, a reset function and a decrement operator.

--- ClickCounter.sol
+++ ClickCounter.sol
@@ -1,13 +1,25 @@
-// SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
-contract ClickCounter{
+contract YesNoCounter{
+    // Unsigned Integer and Anyone Can View It
+    // This is a state variable since it is out of the function
+    uint256 public counter;
 
-    uint256 public counter ;
-
-    function click() public 
-    {
-        counter ++;
+    // Function to Increment Counter
+    function Yes() public { // Gas Fee -->
+        counter++;
     }
+
+    // Function to Decrease Counter
+    function No() public {
+        counter--;
+    }
+
+    // Function to Reset Counter
+    function Reset() public {
+        counter = 0;
+    }
+
 }
